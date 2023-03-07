030364b29d3f585ac99fc01bc212618b269fcd27
Thomas Berube-lambert <tberube-@c2r2p4.42quebec.com>
2023-03-07 15:16:08 -0500

    map square done

--- srcs/init_values.c
+++ srcs/init_values.c
@@ -36,7 +36,7 @@ void	square_map(t_game *game)
 		i++;
 	}
 	i = 0;
-	game->square_map = ft_calloc(game->map_height, sizeof(char *));
+	game->square_map = ft_calloc(game->map_height + 1, sizeof(char *));
 	while (i < game->map_height)
 	{
 		fill_square_map(i, j, game);
@@ -46,23 +46,17 @@ void	square_map(t_game *game)
 
 void	fill_square_map(int i, int j, t_game *game)
 {
-	if ((int)ft_strlen(game->map[i]) < game->max_line)
+	game->square_map[i] = ft_calloc(game->max_line, sizeof(char));
+	while (j < (game->max_line - 1))
 	{
-		game->square_map[i] = ft_calloc(game->max_line + 1, sizeof(char));
-		while (game->map[i][j] != '\n')
-		{
-			game->square_map[i][j] = game->map[i][j]; 
-			j++;
-		}
-		while (j < game->max_line)
-		{
+		if (game->map[i][j] == ' ' || game->map[i][j] == '	' || game->map[i][j] == '0' \
+		|| game->map[i][j] == '\n' || game->map[i][j] == '1')
 			game->square_map[i][j] = '1';
-			j++;
-		}
-		game->square_map[i][j] = '\n';
+		else if (game->map[i][j] == 'Q')
+			game->square_map[i][j] = 'Q';
+		else
+			game->square_map[i][j] = '1';
+		j++;
 	}
-	else
-	{
-		game->square_map[i] = ft_strdup(game->map[i]);
-	}
+	game->square_map[i][j] = '\n';
 }
